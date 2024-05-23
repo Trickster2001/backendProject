34aa39eb76d537e799af1ca46aba94bda2dad5ba
Trickster2001 <keyurmurkar@gmail.com>
2024-05-23 22:56:07 +0530

    Utilities added

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGODB_URL)
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
     console.log(`\n mongodb connected DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 
 dotenv.config({
@@ -8,4 +9,15 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+  app.on("error", (error) => {
+    console.log("Error in app.on method", error);
+  })
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("App listening on port: ", process.env.PORT);
+  })
+})
+.catch((err) => {
+  console.log("Mongo db connection failed ", err);
+})
